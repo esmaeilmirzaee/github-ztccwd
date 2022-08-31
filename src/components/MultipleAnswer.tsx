332533b03734f353e102ff8ecd9c213373d2cbfb
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2022-09-01 00:57:08 +0430

    1 file modified

--- src/components/MultipleAnswer.tsx
+++ src/components/MultipleAnswer.tsx
@@ -1,3 +1,5 @@
+import { useReducer } from 'react';
+
 type AnswerType = {
   letter: string;
   content: string;
@@ -8,7 +10,21 @@ export type MultipleAnswerType = {
   correctAnswer: string;
 };
 
+enum ACTION {
+  'CLICKED',
+  'WAITING',
+}
+type ActionPropsType = { type: ACTION; payload: string | never };
+type StatePropsType = { clicked: boolean; selected: string };
 
+function clickedReducer(state: StatePropsType, action: ActionPropsType) {
+  switch (action.type) {
+    case ACTION.CLICKED:
+      return { clicked: true, selected: action.payload };
+    default:
+      throw new Error('Action type is undefined');
+  }
+}
 
 function RadioAnswerItem({
   item,
@@ -16,6 +32,11 @@ function RadioAnswerItem({
   item: AnswerType;
   correctAnswer: string;
 }) {
+  const [state, dispatch] = useReducer(clickedReducer, {
+    clicked: false,
+    selected: '',
+  });
+
   return (
     <div className="flex px-10 py-2 hover:bg-indigo-500 cursor-pointer items-center">
       <div className="relative flex border border-indigo-200 rounded-full w-4 h-4">
@@ -37,6 +58,7 @@ function MultipleAnswer({
     <>
       {answerOptions.answers.map((item) => (
         <RadioAnswerItem
+          key={item.letter}
           item={item}
           correctAnswer={answerOptions.correctAnswer}
         />
