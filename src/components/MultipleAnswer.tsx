f0b559fb663e71ae21f9183e5119f7c3887aea80
esmaeilmirzaee <esmaeilmirzaee@gmail.com>
2022-09-01 02:36:44 +0430

    1 file modified

--- src/components/MultipleAnswer.tsx
+++ src/components/MultipleAnswer.tsx
@@ -30,25 +30,38 @@ function clickedReducer(state: StatePropsType, action: ActionPropsType) {
 function RadioAnswerItem({
   item,
   correctAnswer,
+  state,
+  dispatch,
 }: {
   item: AnswerType;
   correctAnswer: string;
+  state;
+  dispatch;
 }) {
-  const [state, dispatch] = useReducer(clickedReducer, {
-    clicked: false,
-    selected: '',
-  });
-
   return (
     <div
-      className="flex px-10 py-2 hover:bg-indigo-500 cursor-pointer items-center"
+      className={`flex px-10 py-2 items-center cursor-pointer ${
+        state.clicked
+          ? correctAnswer === item.letter || item.letter === state.selected
+            ? (correctAnswer === item.letter && 'bg-teal-500') ||
+              (state.selected === item.letter &&
+                'bg-rose-500 cursor-not-allowed')
+            : ''
+          : null
+      }`}
       onClick={() => dispatch({ type: ACTION.CLICKED, payload: item.letter })}
     >
-      <div className="relative flex border border-indigo-200 rounded-full w-4 h-4">
-        {state.selected === correctAnswer ? (
-          <CheckCircleIcon className="w-4 h-4" />
+      <div className="relative flex border border-indigo-200 rounded-full w-5 h-5">
+        {state.clicked ? (
+          correctAnswer === item.letter ? (
+            <CheckCircleIcon className="w-5 h-5" />
+          ) : (
+            <XCircleIcon className="w-5 h-5" />
+          )
         ) : (
-          <XCircleIcon className="w-4 h-4" />
+          <span className="absolute top-1/2 -translate-y-[50%] bg-indigo-300 left-0 -translate-x-[100%] px-2 rounded">
+            {item.letter}
+          </span>
         )}
       </div>
       <span className="px-2">{item.content}</span>
@@ -61,6 +74,11 @@ function MultipleAnswer({
 }: {
   answerOptions: MultipleAnswerType;
 }) {
+  const [state, dispatch] = useReducer(clickedReducer, {
+    clicked: false,
+    selected: '',
+  });
+
   return (
     <>
       {answerOptions.answers.map((item) => (
@@ -68,6 +86,8 @@ function MultipleAnswer({
           key={item.letter}
           item={item}
           correctAnswer={answerOptions.correctAnswer}
+          state={state}
+          dispatch={dispatch}
         />
       ))}
     </>
